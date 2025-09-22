6f7985345cb70523efe5a051d99420783a354770
Firsoova08 <foxalishca@mail.ru>
2025-09-22 13:37:40 +0300

    задание3

--- lab2_Homework_Firsova/Program.cs
+++ lab2_Homework_Firsova/Program.cs
@@ -15,18 +15,33 @@
             //    Console.WriteLine("Число не четное");
             //}
             //задание2
-            Console.Write("Придумайте пароль:");
-            string pin1=Console.ReadLine();
-            Console.Write("Напишите пароль:");
-            string pin2=Console.ReadLine();
-            if (pin1==pin2)
-            {
-                Console.WriteLine("Пароль принят");
-            }
-            else
-            {
-                Console.WriteLine("Пароль не принят");
-            }
+            //Console.Write("Придумайте пароль:");
+            //string pin1=Console.ReadLine();
+            //Console.Write("Напишите пароль:");
+            //string pin2=Console.ReadLine();
+            //if (pin1==pin2)
+            //{
+            //    Console.WriteLine("Пароль принят");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Пароль не принят");
+            //}
+
+            //задание3
+            //int number = int.Parse(Console.ReadLine());
+            //int num1 = number / 1000;
+            //int num2 = (number / 100) % 10;
+            //int num3 = (number / 10) % 10;
+            //int num4 = number % 10;
+            //if (num1 + num4 == num2 - num3)
+            //{
+            //    Console.WriteLine("Да");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Нет");
+            //}
         }
     }
 }
