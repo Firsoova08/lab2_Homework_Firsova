865494fbba01cef8ffc5be947da23e6954e14122
Firsoova08 <foxalishca@mail.ru>
2025-09-22 13:21:04 +0300

    Add project files.

---
+++ lab2_Homework_Firsova/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace lab2_Homework_Firsova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание 1
+            //int a=int.Parse(Console.ReadLine());
+            //if (a % 2 == 0)
+            //{
+            //    Console.WriteLine("Число четное");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Число не четное");
+            //}
+        }
+    }
+}
